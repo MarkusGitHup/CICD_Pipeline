2235c22154725d07bb1eab719abe018090cdf291
MarkusGitHup <knauer.markus@web.de>
2019-04-30 15:29:44 +0200

    add current files

--- Parcelsize/src/main/ParcelsizeServer.java
+++ Parcelsize/src/main/ParcelsizeServer.java
@@ -1,12 +1,30 @@
 package main;
 
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.net.httpserver.HttpServer;
+
 public class ParcelsizeServer {
 
-	public static void main(String[] args) {
+	public static void main(String[]args) {
 		
-	System.out.println("Hello Parcelservice!");
-	
-
+		HttpServer server;
+		try {
+			server = HttpServerFactory.create("http://localhost:8082/rest");
+			server.start();
+			JOptionPane.showMessageDialog(null, "Ende");
+			server.stop(0);
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
--- Parcelsize/src/main/RestService.java
+++ Parcelsize/src/main/RestService.java
@@ -5,8 +5,11 @@ import java.io.IOException;
 import javax.swing.JOptionPane;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import com.sun.jersey.api.container.httpserver.HttpServerFactory;
 import com.sun.net.httpserver.HttpServer;
@@ -20,22 +23,57 @@ public class RestService {
 		return "hello";
 
 	}
-
-public static void main(String[]args) {
-	HttpServer server;
-	try {
-		server = HttpServerFactory.create("http://localhost:8080/rest");
-		server.start();
-		JOptionPane.showMessageDialog(null, "Ende");
-		server.stop(0);
-	} catch (IllegalArgumentException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
 	
+	@GET
+	@Produces( MediaType.APPLICATION_JSON )
+	@Path("length/{length}/width/{width}/height/{height}")
+	public Response message( @PathParam("length") int length,
+	                       @PathParam("width") int width,
+	                       @PathParam("height") int height)
+	{
+	  int gurtmas = (height + width)*2 + length;
+	  OutputObject retVal = new OutputObject();
+	  retVal.length = length;
+	  retVal.width = width;
+	  retVal.height = height;
+	  if (0 <= gurtmas && gurtmas < 35)
+	  {
+		  retVal.size = "XS";
+	  }
+	  else if (35 <= gurtmas && gurtmas < 50)
+	  {
+		  retVal.size = "S";
+	  
+	  }
+	  else if (50 <= gurtmas && gurtmas < 65)
+	  {
+		  retVal.size = "M";
+  
+	  }
+	  else if (65 <= gurtmas && gurtmas < 80)
+	  {
+		  retVal.size = "L";
+ 
+	  }
+	  else if (80 <= gurtmas && gurtmas < 300)
+	  {
+		  retVal.size = "XL";
+
+	  }
+	  return Response.ok()
+              .entity(retVal)
+              .header("Access-Control-Allow-Origin", "*")
+              .build();
+
+	}
 }
+
+@XmlRootElement
+class OutputObject
+{
+	public int length;
+	public int width;
+	public int height;
+	public String size;
 }
 
